8c171e86fe8c4bae17a260bd89c2235594b500a0
chengshuangfeng <chengshuangfeng@kuaishou.com>
2022-03-13 01:15:18 +0800

    feat: 修改sh文件

--- shell/deploy-csf.sh
+++ shell/deploy-csf.sh
@@ -2,12 +2,12 @@
 # 在这里编写项目的提测脚本
 #!/bin/sh
 rm -rf docs/.vuepress/dist
-
-name=vuepress-audition
+dns=audition
+name=vuepress-$dns
 echo building project $name:
 # 打包
 npm run docs:build
 # 发布
 rsync -arz docs/.vuepress/dist/ root@www.hu77.top:/root/home/$name/
 
-echo you can visit http://blog.hu77.top/
+echo you can visit http://$dns.hu77.top/
