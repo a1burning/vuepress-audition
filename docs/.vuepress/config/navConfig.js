cad5627039a2d1e8ad18b9bf10d0b3f293666029
chengshuangfeng <chengshuangfeng@kuaishou.com>
2022-03-30 23:04:03 +0800

    feat: add gc

--- docs/.vuepress/config/navConfig.js
+++ docs/.vuepress/config/navConfig.js
@@ -1,9 +1,12 @@
 const home = { text: 'Home', link: '/', icon: 'reco-home' }
 
-const computer = { text: '计算机网络', link: '/src/computer/01.md' }
+const computer = { text: '计算机基础', items: [
+  { text: '计算机网络', link: '/src/computer/network/01.md' },
+  { text: '垃圾回收', link: '/src/computer/gc/01.md' },
+]}
 
 const frontend = { text: '前端基础', items: [
-  { text: '浏览器渲染', link: '/src/base/render/01.md' },
+  { text: '浏览器', link: '/src/base/render/01.md' },
   { text: '前端基础', items: [
     { text: 'js', link: '/src/base/javaScript/01.md' },
     { text: 'css', link: '/src/base/css/01.md' }
